59259f4bf6aee5262569443eb60169144a51dadb
Mohil Tharecha <jaydipupadhyay5@gmail.com>
2023-10-20 18:44:16 +0530

    changed strture of folders

---
+++ projects/Master Assets/JS/masterjs.js
@@ -0,0 +1,17 @@
+function genLink(){
+    // Get the UL element where you want to add the links
+    const projectList = document.getElementById('project-list');
+    
+    // Define your project folder names
+    const projectFolders = ['Meal-Management', 'HTML-PDF', 'IAN','Fig-Plugin'];
+
+    // Iterate over the project folders and create links
+    projectFolders.forEach(folderName => {
+        const listItem = document.createElement('li');
+        const link = document.createElement('a');
+        link.href = `./projects/${folderName}/index.html`;
+        link.textContent = folderName;
+        listItem.appendChild(link);
+        projectList.appendChild(listItem);
+    });
+}
