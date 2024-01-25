d80a9e5efe55ef9334af928bb6074139773d5152
jdip19 <ujaydip9@gmail.com>
2024-01-25 23:59:07 +0530

    dynamic price list

--- Master Assets/JS/masterjs.js
+++ Master Assets/JS/masterjs.js
@@ -3,7 +3,7 @@ function genLink(){
     const projectList = document.getElementById('project-list');
     
     // Define your project folder names
-    const projectFolders = ['Meal-Management', 'Front-End', 'IAN','Fig-Plugin','Batch-Files','ClockSheet','my-static-website','hidiary'];
+    const projectFolders = ['Meal-Management', 'Front-End', 'IAN','Fig-Plugin','Batch-Files','ClockSheet','my-static-website','hidiary','J-K-Provision'];
 
     // Iterate over the project folders and create links
     projectFolders.forEach(folderName => {
