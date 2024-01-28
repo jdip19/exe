a4cef73ca30c7b3d44d63f684799a654a0f549c7
ujaydip19@gmail.com <ujaydip19@gmail.com>
2024-01-28 14:52:50 +0530

    added database config

---
+++ projects/J-K-Provision/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYF1MoFUCozgh6PfsH-nM1avUTbxSM_rY",
+  authDomain: "my-store-11-6b8f5.firebaseapp.com",
+  databaseURL:"https://my-store-11-6b8f5-default-rtdb.firebaseio.com",
+  projectId: "my-store-11-6b8f5",
+  storageBucket: "my-store-11-6b8f5.appspot.com",
+  messagingSenderId: "719774944841",
+  appId: "1:719774944841:web:9ac216f8ffb2e49bcc7998"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- projects/J-K-Provision/main.js
+++ projects/J-K-Provision/main.js
@@ -1,10 +1,15 @@
-$('.has-clear input[type="text"]').on('input propertychange', function() {
-    var $this = $(this);
-    var visible = Boolean($this.val());
-    $this.siblings('.form-control-clear').toggleClass('hidden', !visible);
-  }).trigger('propertychange');
-  
-  $('.form-control-clear').click(function() {
-    $(this).siblings('input[type="text"]').val('')
-      .trigger('propertychange').focus();
+//opening edit model
+$(document).ready(function () {
+  // Handle double-click on the card
+  $('#clickable').on('dblclick', function () {
+      // Extract the ID from the clicked card
+      var cardId = $(this).find('.card-id span').text();
+
+      // Update the modal content with the ID
+      $('#modalCardId').text(cardId);
+
+      // Show the modal
+      $('#editCardModal').modal('show');
   });
+});
+
