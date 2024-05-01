b39069ee05686ae0cb2f342324ec90c9d2668e38
jdip19 <ujaydip9@gmail.com>
2024-05-02 00:30:50 +0530

    FInally Most of case handled in title case and sentace case

--- projects/Fig-Plugin/QuickText/code.ts
+++ projects/Fig-Plugin/QuickText/code.ts
@@ -27,16 +27,24 @@ if (textNodes.length === 0) {
 }
 
 function handleTextCase(node: TextNode): void {
-  let newText;
+  let newText=node.characters;
   switch (figma.command) {
     case 'titlecase':
-      const conjunctions = ['for', 'in', 'on', 'of', 'am', 'are', 'and', 'to', 'is', 'at', 'also',];
-      newText = node.characters; // Assuming node.characters contains the text
+      const conjunctions = ['for', 'an', 'a', 'in', 'on', 'of', 'am', 'are', 'and', 'to', 'is', 'at', 'also'];
+    
+
+      const TitleCase = newText.split(' ').every(word => {
+        const firstLetter = word.charAt(0);
+        const restOfWord = word.slice(1);
+        // Check if the word has title case
+        return firstLetter.toUpperCase() === firstLetter && restOfWord.toLowerCase() === restOfWord;
+      });
+
       newText = newText.replace(/\b(\w+)\b/g, (match, word) => {
         if (word.toUpperCase() === word || conjunctions.indexOf(word.toLowerCase()) !== -1) {
           return word;
         } else {
-          return match.charAt(0).toUpperCase() + match.slice(1).toLowerCase(); // Convert to sentence case
+          return match.charAt(0).toUpperCase() + match.slice(1).toLowerCase();
         }
       });
       if (newText === newText.toUpperCase()) {
@@ -49,6 +57,18 @@ function handleTextCase(node: TextNode): void {
             return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
           }
         });
+      } else if (TitleCase) {
+  
+        newText = newText.toLowerCase(); // Convert to lowercase
+        newText = newText.replace(/\b(\w+)\b/g, (match, word) => {
+          // Check if the word is a conjunction, then keep it lowercase
+          if (conjunctions.indexOf(word.toLowerCase()) !== -1) {
+
+            return word.toLowerCase();
+          } else {
+            return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+          }
+        });
       }
       node.characters = newText; // Update the node with the modified text
       figma.notify('Tadaannn... 🥁 Your Text case changed to TitleCase.');
@@ -56,21 +76,49 @@ function handleTextCase(node: TextNode): void {
 
     case 'sentencecase':
       // Convert the text to lowercase first
-      newText = node.characters.toLowerCase();
+
+      const allUppercase = newText.split(' ').every(word => word.toUpperCase() === word);
+
 
       // Define the regular expression pattern to match the beginning of sentences
-      const sentenceRegex = /(^|[.!?]\s+)(\w+)/g;
+      let titleCaseCount = 0; // Initialize a counter for title case words
 
-      // Replace the matched patterns with capitalized letters
-      newText = newText.replace(sentenceRegex, (match, boundary, word) => {
-        // Check if the word is an acronym (all uppercase), if so, keep it as it is
-        const isAcronym = word.length > 1 && word === word.toUpperCase();
-        if (isAcronym) {
-          return match;
+      newText.split(' ').every(word => {
+        const firstLetter = word.charAt(0);
+        const restOfWord = word.slice(1);
+        // Check if the word has title case
+        if (firstLetter.toUpperCase() === firstLetter && restOfWord.toLowerCase() === restOfWord) {
+          titleCaseCount++; // Increment the title case word count
+          return true;
         } else {
-          return boundary + word.charAt(0).toUpperCase() + word.slice(1);
+          return false;
         }
       });
+      //console.log(titleCaseCount);
+
+      if (allUppercase) {
+        newText = newText.toLowerCase().charAt(0).toUpperCase() + newText.slice(1).toLowerCase();
+        //console.log("allUppercase true");
+
+      }
+      else if (titleCaseCount >= 2) {
+        newText = newText.toLowerCase().replace(/(^|[.!?]\s+)(\w+)/g, firstLetter => firstLetter.charAt(0).toUpperCase() + firstLetter.slice(1).toLocaleLowerCase());
+      } else {
+        // Define the regular expression pattern to match the beginning of sentences
+        const sentenceRegex = /(^|[.!?]\s+)(\w+)/g;
+
+        // Replace the matched patterns with capitalized letters
+        newText = newText.replace(sentenceRegex, (match, boundary, word) => {
+          // Check if the word is an acronym (all uppercase), if so, keep it as it is
+          const isAcronym = word.length > 1 && word.toUpperCase() === word;
+          if (isAcronym) {
+            return boundary + word; // Keep the acronym as it is
+          } else {
+            return boundary + word.charAt(0).toUpperCase() + word.slice(1).toLowerCase(); // Convert to Sentence Case
+          }
+        });
+      }
+
       node.characters = newText; // Update the node with the modified text
       figma.notify('Tadaannn... 🥁 Your Text case changed to Sentencecase.');
       break;
@@ -86,8 +134,12 @@ function handleTextCase(node: TextNode): void {
       break;
 
     case 'addbreakline':
-      node.characters = node.characters.replace(/\. ?([A-Z])/g, '.\n$1'); // Add a line break after a full stop followed by an optional space and uppercase letter
-      figma.notify('Tadaannn... 🥁 Your Text now has line breaks after sentences.');
+      node.characters = node.characters.replace(/\. ?([a-z]|[A-Z])/g, '.\n$1');
+      node.characters = node.characters.replace(/(^\w|\. ?\w)/gm, (match) => {
+        return match.toUpperCase();
+      });
+      // Add a line break after a full stop followed by an optional space and uppercase letter
+      figma.notify('Tadaannn... 🥁 Your Text now has line breaks after Fullstop.');
       break;
 
     case 'rmvbreakline':
