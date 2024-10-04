953db9d90fc605286530f63c322c2880318feefd
Mohil Tharecha <jaydipupadhyay5@gmail.com>
2024-10-04 09:18:42 +0530

    loop understand

--- projects/JS-L/main.js
+++ projects/JS-L/main.js
@@ -1,13 +1,15 @@
-let i=1,j=1;
+let i=1,j=1,k=2;
 let op="";
 
 
-for(i;i<=9;i++){
-    for(j;j<=3;j++)
+for(i;i<=3;i++){
+    for(j;j<=k;j++)
     {
         op+=j+" ";
     }
+   
     op+="\n";
+    k=k+2;
 }
 
 document.getElementById("op").textContent=op;
