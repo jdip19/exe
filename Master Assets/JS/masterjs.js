b287ee3449782e4b728ef6f5697efd628a41307e
Mohil Tharecha <jaydipupadhyay5@gmail.com>
2024-10-10 18:22:35 +0530

    updated

--- Master Assets/JS/masterjs.js
+++ Master Assets/JS/masterjs.js
@@ -1,19 +1,32 @@
-function genLink(){
+function genLink() {
     // Get the UL element where you want to add the links
     const projectList = document.getElementById('project-list');
     
-    // Define your project folder names
-    const projectFolders = ['Meal-Management', 'Front-End', 'IAN','Fig-Plugin','Batch-Files','ClockSheet','my-static-website','hidiary','J-K-Provision','JS-L','UDownloader'];
+    // Define your project folder names and their corresponding ZIP file names
+    const projectFolders = [
+        { folderName: 'Meal-Management', zipFile: 'Meal-Management.zip' },
+        { folderName: 'Front-End', zipFile: 'Front-End.zip' },
+        { folderName: 'IAN', zipFile: 'IAN.zip' },
+        { folderName: 'Fig-Plugin', zipFile: 'Fig-Plugin.zip' },
+        { folderName: 'Batch-Files', zipFile: 'Batch-Files.zip' },
+        { folderName: 'ClockSheet', zipFile: 'ClockSheet.zip' },
+        { folderName: 'my-static-website', zipFile: 'my-static-website.zip' },
+        { folderName: 'hidiary', zipFile: 'hidiary.zip' },
+        { folderName: 'J-K-Provision', zipFile: 'J-K-Provision.zip' },
+        { folderName: 'JS-L', zipFile: 'JS-L.zip' },
+        { folderName: 'UDownloader', zipFile: 'UDownloader.zip' }
+    ];
 
     // Iterate over the project folders and create links
-    projectFolders.forEach(folderName => {
-        const icon =document.createElement('i')
-        icon.className="fi fi-rr-arrow-small-right";
+    projectFolders.forEach(project => {
+        const icon = document.createElement('i');
+        icon.className = "fi fi-rr-arrow-small-right";
         const listItem = document.createElement('li');
         const link = document.createElement('a');
-        link.href = `./projects/${folderName}/index.html`;
-        link.textContent = folderName;
-        listItem.appendChild(icon)
+        link.href = `./projects/${project.zipFile}`;  // Point to the ZIP file
+        link.download = project.zipFile; // Set the download attribute for the link
+        link.textContent = project.folderName;
+        listItem.appendChild(icon);
         listItem.appendChild(link);
         projectList.appendChild(listItem);
     });
